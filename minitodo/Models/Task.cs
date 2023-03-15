1d06ecad828b0faeb2c804c0104b754124f544d1
CvCvCvv <decomen123@gmail.com>
2023-03-15 17:28:48 +0500

    add task

---
+++ minitodo/Models/Task.cs
@@ -0,0 +1,10 @@
+﻿namespace minitodo.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = null!;
+        public bool IsDone { get; set; }
+        public bool IsFavorite { get; set; }
+    }
+}
