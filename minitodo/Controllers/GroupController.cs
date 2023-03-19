5921cf6baf56d43e7f88660b2e9ac7559e02078d
CvCvCvv <decomen123@gmail.com>
2023-03-19 21:08:35 +0500

    add group controller

--- minitodo/Controllers/GroupController.cs
+++ minitodo/Controllers/GroupController.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using minitodo.Models;
-
+using System.Text.RegularExpressions;
+using minitodo.Dtos;
 namespace minitodo.Controllers
 {
     public class GroupController : Controller
@@ -12,44 +13,36 @@ namespace minitodo.Controllers
             _context = context;
         }
 
-        public string Putin()
-        {
-            return "Vor";
-        }
         [HttpPost]
         public JsonResult Create(string nameGroup)
         {
-            return Json(new{name = nameGroup });
-
             var user = _context.Users.Find(1);
             if (nameGroup == null)
-                return null;
+                return Json(false);
 
-            var group = new Group { Name = nameGroup, User = user };
+            var group = new Models.Group { Name = nameGroup, User = user };
             _context.Groups.Add(group);
             _context.SaveChanges();
             return Json(nameGroup);
         }
         [HttpPost]
-        [ValidateAntiForgeryToken]
         public JsonResult Delete(int id)
         {
             var user = _context.Users.Find(1);
             var group = _context.Groups.Find(id);
             if (group == null || group.UserId != user.Id)
-                return null;
+                return Json(false);
             _context.Groups.Remove(group);
             _context.SaveChanges();
             return Json(true);
         }
         [HttpPost]
-        [ValidateAntiForgeryToken]
         public JsonResult Edit(int id, string nameGroup)
         {
             var user = _context.Users.Find(1);
             var group = _context.Groups.Find(id);
-            if (nameGroup == null || group==null||group.UserId!=user.Id)
-                return null;
+            if (nameGroup == null || group == null || group.UserId != user.Id)
+                return Json(false);
 
             group.Name = nameGroup;
             _context.SaveChanges();
@@ -59,8 +52,12 @@ namespace minitodo.Controllers
         public JsonResult Read()
         {
             var user = _context.Users.Find(1);
-            var groupList = _context.Groups.Include(a => a.Tasks).Where(a => a.UserId == user.Id);
-
+            var groups = _context.Groups.Where(a => a.UserId == user.Id).ToList();
+            var groupList = new List<GroupModel>();
+            foreach (var group in groups)
+            {
+                groupList.Add(new GroupModel() { Id = group.Id, Name = group.Name });
+            }
             return Json(groupList);
         }
     }
---
+++ minitodo/Dtos/GroupModel.cs
@@ -0,0 +1,8 @@
+﻿namespace minitodo.Dtos
+{
+    public class GroupModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
