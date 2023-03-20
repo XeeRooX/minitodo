e55ab2ce99bab59ddd4dfccd9932b1faf9c98001
XeeRooX <akkaunt7931@gmail.com>
2023-03-20 21:16:38 +0500

    first layaout html changes

---
+++ minitodo/wwwroot/js/master.js
@@ -0,0 +1,10 @@
+﻿async function main() {
+    $("#add-task-group-btn").click(await AddTaskHandler);
+}
+
+async function AddTaskHandler() {
+    $("#cancel-btn-div").removeClass("d-none");
+    $("#name-input-group").removeClass("d-none");
+    $("#add-task-btn-div").addClass("d-none");
+}
+main();
