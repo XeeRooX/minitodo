49cc94374d8c1ec4dae96e37d1bec8a64c500511
XeeRooX <akkaunt7931@gmail.com>
2023-03-21 22:05:30 +0500

    Update layout.html

--- minitodo/wwwroot/js/master.js
+++ minitodo/wwwroot/js/master.js
@@ -1,8 +1,22 @@
 ﻿async function main() {
     $("#add-task-group-btn").click(await AddTaskHandler);
+    $("#cancel-btn").click(await CancelTaskAddHandler);
+    $("#add-grpup-btn").click(await AddGroupHandler);
+}
+
+async function AddGroupHandler() {
+    var groupName = $("#name-input").val();
+    alert(groupName);
+}
+
+async function CancelTaskAddHandler() {
+    $("#add-task-btn-div").removeClass("d-none");
+    $("#name-input-group").addClass("d-none");
+    $("#cancel-btn-div").addClass("d-none");
 }
 
 async function AddTaskHandler() {
+    $("#name-input").val("");
     $("#cancel-btn-div").removeClass("d-none");
     $("#name-input-group").removeClass("d-none");
     $("#add-task-btn-div").addClass("d-none");
