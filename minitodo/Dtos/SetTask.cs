dfd07b172615570d98d099d50dc2aa36091759f8
XeeRooX <akkaunt7931@gmail.com>
2023-03-27 09:38:01 +0500

    Add and set TaskController

---
+++ minitodo/Dtos/SetTask.cs
@@ -0,0 +1,7 @@
+﻿namespace minitodo.Dtos
+{
+    public class SetTask
+    {
+        public int TaskId { get; set; }
+    }
+}
---
+++ minitodo/Dtos/TaskDelete.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace minitodo.Dtos
+{
+    public class TaskDelete
+    {
+        [Required]
+        public int TaskId { get; set; }
+    }
+}
