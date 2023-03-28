36bfeb84fb16ab7f74915920be6534fa640f624c
CvCvCvv <decomen123@gmail.com>
2023-03-28 19:43:39 +0500

    add edit group

--- minitodo/wwwroot/js/master.js
+++ minitodo/wwwroot/js/master.js
@@ -2,7 +2,9 @@
     $("#add-task-group-btn").click(await AddTaskHandler);
     $("#cancel-btn").click(await CancelTaskAddHandler);
     $("#add-grpup-btn").click(await AddGroupHandler);
-    $(".groups").on('click',".del-btn", DeleteItem);
+    $(".groups").on('click', ".del-btn", DeleteItem);
+    $(".groups").on('click', ".edit-btn", await EditNameGroup);
+    $(".groups").on('click', ".save-edit-btn", await SaveEditGroup);
     await GetAllGroups();
 }
 
@@ -29,7 +31,7 @@ function DeleteItem() {
             console.log("ok " + idDel);
         }).fail(function (xhr, textStatus, errorThrown) { alert("error: " + xhr.responseText); })
         ;
-    $(".groups").find("#" + idDel).parent().remove();
+    $(".groups").children("#" + idDel).remove();
 }
 
  function AddGroupToList(groupJson)
@@ -39,12 +41,37 @@ function DeleteItem() {
      group.find('.name-group').text(groupJson.name);
      group.find('.name-group').attr('id', groupJson.id);
      group.find('.del-btn').attr('id', groupJson.id);
+     group.find('.edit-panel').attr('id', groupJson.id);
+     group.attr('id', groupJson.id);
     group.removeClass('d-none');
     group.removeClass('group-example');
     $(".groups").append(group);
 
 }
 
+async function EditNameGroup() {
+    $(this).parent().addClass("d-none");
+    $(this).parent().next().removeClass("d-none");
+    $(this).parent().next().children('.form-control').val($(this).parent().find(".name-group").text());
+}
+async function returnToDefaultPanel(name, id) {
+    var dom = $('#' + id);
+    dom.children('.default-panel').removeClass("d-none");
+    dom.children('.default-panel').children('.name-group').text(name);
+    dom.children('.edit-panel').addClass("d-none");
+}
+async function SaveEditGroup() {
+    var newNameGroup = $(this).closest(".edit-panel").children('.form-control').val();
+    var idGroup = $(this).closest(".edit-panel").attr("id");
+    console.log(idGroup);
+
+    $.post("Group/Edit", { id: idGroup, nameGroup: newNameGroup })
+        .done(function (data, statusText) {
+            console.log("ok " + data);
+            returnToDefaultPanel(data, idGroup);
+        }).fail(function (xhr, textStatus, errorThrown) { alert("error: " + xhr.responseText); })
+        ;
+}
 async function GetAllGroups() {
     console.log('sdfsf');
     $.get("Group/Read")
