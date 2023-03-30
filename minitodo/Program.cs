263ac0da4f90898dd8f524d5a51ea80718540721
XeeRooX <akkaunt7931@gmail.com>
2023-03-31 00:19:11 +0500

    Add auto create database

--- minitodo/Program.cs
+++ minitodo/Program.cs
@@ -10,6 +10,8 @@ builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 string connectionString = builder.Configuration.GetConnectionString("DefaultConntection")!;
 builder.Services.AddDbContext<AppDbContext>(
     options=>options.UseSqlite(connectionString));
+
+
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
     {
@@ -17,7 +19,14 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
         options.AccessDeniedPath = "/accessdenied";
     });
 builder.Services.AddAuthorization();
+
 var app = builder.Build();
+
+
+using (var scope =
+  app.Services.CreateScope())
+using (var context = scope.ServiceProvider.GetService<AppDbContext>())
+    context.Database.Migrate();
 
 if (!app.Environment.IsDevelopment())
 {
